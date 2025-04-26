e31a4bd7a82f554008778a122b409977a614355f
Raheel <raheelalam77@gmail.com>
2025-04-26 13:23:12 +0500

    Update Files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
     document.querySelector('.nav-center').classList.toggle('show');
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const bookmarkIcons = document.querySelectorAll('.bookmark-icon');
+
+    bookmarkIcons.forEach(function (icon) {
+        icon.addEventListener('click', function () {
+            icon.classList.toggle('active');
+        });
+    });
+});
